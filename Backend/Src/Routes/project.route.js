c70036bc115d5fc9e68e65dba6d63fa962cac8d0
Rishu Raj <rishuraj1127@gmail.com>
2025-07-17 10:51:50 +0530

    Project Route

--- Backend/Src/Routes/project.route.js
+++ Backend/Src/Routes/project.route.js
@@ -1,4 +1,4 @@
-import {getAllProjects , createProject} from "../Controller/Project.controller.js"
+import {getAllProjects , createProject , getTaskOfProject} from "../Controller/Project.controller.js"
 import { Router } from "express";
 import { verifyJWT } from "../Middleware/Auth.middleware.js";
 import {orgverifyJWT} from "../Middleware/OrgAuth.middleware.js";
@@ -9,6 +9,8 @@ const router = Router();
 
 router.route("/createProject").post(orgverifyJWT, createProject);
 router.route("/getAllProjects").get(orgverifyJWT, getAllProjects);
+router.route("/getTaskOfProject/:projectId").get( getTaskOfProject);
+
 
 
 
