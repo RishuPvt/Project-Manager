e68d2c0221470af72b286bbc0b53858fea1a3573
Rishu Raj <rishuraj1127@gmail.com>
2025-07-19 09:53:41 +0530

    Task Router

--- Backend/Src/Routes/task.route.js
+++ Backend/Src/Routes/task.route.js
@@ -1,15 +1,18 @@
-import {createTask , getmyTask , updateTaskStatus} from "../Controller/task.controller.js"
+import {createTask , getmyTask , updateTaskStatus , getTaskStatusCount} from "../Controller/task.controller.js"
 import { Router } from "express";
 import { verifyJWT } from "../Middleware/Auth.middleware.js";
 import {orgverifyJWT} from "../Middleware/OrgAuth.middleware.js";
+import { upload } from "../Middleware/multer.middleware.js";
 
 
 const router = Router();
 
 
-router.route("/createTask/:projectId").post(orgverifyJWT, createTask);
+router.route("/createTask/:projectId").post(orgverifyJWT,upload.single("file") ,createTask);
 router.route("/getmyTask").get(verifyJWT, getmyTask);
 router.route("/updateTaskStatus/:taskId").patch(verifyJWT, updateTaskStatus);
+router.route("/getTaskStatusCount/:projectId").get( getTaskStatusCount);
+
 
 
 
