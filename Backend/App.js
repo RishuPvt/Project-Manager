6987ba30842a7bf8a2c844ee141174233578bad7
Rishu Raj <rishuraj1127@gmail.com>
2025-07-19 09:54:00 +0530

    Updated App

--- Backend/App.js
+++ Backend/App.js
@@ -4,7 +4,7 @@ import cors from "cors";
 const app = express();
 
 const corsOptions = {
-  origin: "https://backend-hub.vercel.app",
+  origin: "http://localhost:5173",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Origin", "Content-Type", "Accept", "Authorization"],
@@ -31,5 +31,7 @@ import TaskRouter from "../Backend/Src/Routes/task.route.js";
 
 app.use("/api/v1/project/task", TaskRouter);
 
+import userRouter from "../Backend/Src/Routes/user.router.js";
 
+app.use("/api/v1/profile" , userRouter)
 export { app };
