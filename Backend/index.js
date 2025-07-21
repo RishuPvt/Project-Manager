a256d27f79ee8f42a3be75dad579c8649c8b92fc
Rishu Raj <rishuraj1127@gmail.com>
2025-07-21 11:56:25 +0530

    Index.js file

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,35 @@
 import {app} from "./App.js";
 import { configDotenv } from "dotenv";
+import { Server } from "socket.io";
+import {createServer} from "http";
+
 configDotenv({ path: "./.env" });
 
-app.listen(process.env.PORT || 8000,()=>{
+
+const server = createServer(app); 
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    credentials: true,
+  },
+});
+
+export { io };
+
+io.on("connection", (socket) => {
+  console.log("🟢 Socket connected:", socket.id);
+
+  // Join room
+  socket.on("join_project", (projectId) => {
+    socket.join(`project_${projectId}`);
+    console.log(`Socket ${socket.id} joined project_${projectId}`);
+  });
+});
+
+
+server.listen(process.env.PORT || 8000,()=>{
  console.log(`Server is running at port :${process.env.PORT}`);
 })
+
